a3955e15c4bdfe9f6d9b678220af41c385031f45
Adnan Abbasi <adnan_abbasi@yahoo.com>
2023-07-24 22:38:27 -0400

    Initial commit

---
+++ employeemanager/src/main/java/tech/adnanabbasi/employeemanager/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package tech.adnanabbasi.employeemanager.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ employeemanager/src/main/java/tech/adnanabbasi/employeemanager/repo/EmployeeRepo.java
@@ -0,0 +1,12 @@
+package tech.adnanabbasi.employeemanager.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import tech.adnanabbasi.employeemanager.model.Employee;
+
+import java.util.Optional;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
+}
