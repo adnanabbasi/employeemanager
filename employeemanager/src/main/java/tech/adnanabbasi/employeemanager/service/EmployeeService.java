fa6a1bb20cd91cc5941e5ec8a458dc20087908a3
Adnan Abbasi <adnan_abbasi@yahoo.com>
2023-07-25 22:48:08 -0400

    Added transactional to the service so that delete functionality works on the angular app

--- employeemanager/src/main/java/tech/adnanabbasi/employeemanager/service/EmployeeService.java
+++ employeemanager/src/main/java/tech/adnanabbasi/employeemanager/service/EmployeeService.java
@@ -2,6 +2,7 @@ package tech.adnanabbasi.employeemanager.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import tech.adnanabbasi.employeemanager.exception.UserNotFoundException;
 import tech.adnanabbasi.employeemanager.model.Employee;
 import tech.adnanabbasi.employeemanager.repo.EmployeeRepo;
@@ -10,6 +11,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Service
+@Transactional
 public class EmployeeService {
     private final EmployeeRepo employeeRepo;
 
